ec67226837a57865c5c6d91ef8fb93b780916e8f
BigBiba <nikitafedorov.or@gmail.com>
2024-12-22 16:43:15 +0300

    feat(auth) проверка на авторизацию
    Если пользователь не авторизован, но хочет попасть на корневой адрес, то автоматически переходит на страницу авторизации

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(auth_params[:email])
-    if user && user.authenticate(auth_params[:password])
-      session[:current_user_id] = user.id
+    @user = User.find_by_email(auth_params[:email])
+    if @user && @user.authenticate(auth_params[:password])
+      session[:current_user_id] = @user.id
       redirect_to root_path
     else
       redirect_to login_path
     end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_path
   end
 
   private
