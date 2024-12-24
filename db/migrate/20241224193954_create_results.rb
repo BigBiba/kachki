8cccacaf22f129291ad3e37276c09524349440f7
BigBiba <nikitafedorov.or@gmail.com>
2024-12-24 23:42:13 +0300

    rubocop will be proud

--- db/migrate/20241224193954_create_results.rb
+++ db/migrate/20241224193954_create_results.rb
@@ -1,8 +1,8 @@
 class CreateResults < ActiveRecord::Migration[8.0]
   def change
     create_table :results do |t|
-      t.references :athlete, null: false, foreign_key: {to_table: :users}
-      t.references :referee, null: false, foreign_key: {to_table: :users}
+      t.references :athlete, null: false, foreign_key: { to_table: :users }
+      t.references :referee, null: false, foreign_key: { to_table: :users }
       t.references :competition, null: false, foreign_key: true
       t.float :scope
 
